163f886258f3eb3fad7992dfa5d81675722f0285
swathi <swathi.tpg@gamil.com>
2020-05-15 14:37:36 +0530

    first commit

---
+++ gitproject/gitpackage/test_case2.py
@@ -0,0 +1,11 @@
+class A:
+    
+    def method1(self):
+        print("This is method1 in class A")
+    def method2(self):
+        print("This is method2 in class A")
+    def method3(self):
+        print("This is method3 in class A")
+
+a = A()
+a.method1()
