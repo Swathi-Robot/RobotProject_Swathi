4302a50093945eb93617515d2499bdc93fca469e
swathi <swathi.tpg@gamil.com>
2020-05-15 16:30:09 +0530

    added print statement

--- gitproject/gitpackage/test_case2.py
+++ gitproject/gitpackage/test_case2.py
@@ -4,6 +4,8 @@ class A:
         print("This is method1 in class A")
     def method2(self):
         print("This is method2 in class A")
+        print("print -2 This is method2 in class A")
+        
     def method3(self):
         print("This is method3 in class A")
 
