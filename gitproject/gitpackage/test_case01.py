dd10fee6dadd37dae62f752e28ea78155f7e6992
Swathi <swathi.tpg@gmail.com>
2020-07-17 15:08:44 +0530

    [UPDT] changes of print stmt in methiod1 testcase1

--- gitproject/gitpackage/test_case01.py
+++ gitproject/gitpackage/test_case01.py
@@ -1,7 +1,7 @@
 class A:
     
     def method1(self):
-        print("This is method1 in class A")
+        print("This is method1 in esss A")
     def method2(self):
         print("This is method2 in class A")
     def method3(self):
